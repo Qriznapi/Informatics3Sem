325eca69fd57ecaa7ad7ea28332c1e5adf37a3d8
nApi <106438187+Qriznapi@users.noreply.github.com>
2024-09-22 20:41:04 +0300

    Update 4n5.c

--- 4sem/4n5.c
+++ 4sem/4n5.c
@@ -6,7 +6,7 @@ double circles(int a, int b, int x, int y)
     return pow((a-x)*(a-x)+(b-y)*(b-y), 0.5);
 }
 
-int main45()
+int main()
 {
     int a, b, c, x, y, z;
     scanf("%i %i %i", &x, &y, &z);
