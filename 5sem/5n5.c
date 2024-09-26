28dbcaec0195c786839bedb072cd80bef6b3bda2
nApi <106438187+Qriznapi@users.noreply.github.com>
2024-09-26 17:40:58 +0300

    Update 5n5.c

--- 5sem/5n5.c
+++ 5sem/5n5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void trim_after_first_space(char str[]) {
+void trim_after_first_space(char str[100]) {
     int i = 0;
     while (str[i] != ' ' && str[i] != '\0') {
         i++;
@@ -10,7 +10,7 @@ void trim_after_first_space(char str[]) {
 }
 
 int main() {
-    char a[] = "Cats and Dogs";
+    char a[100] = "Cats and Dogs";
     printf("%s\n", a);
     trim_after_first_space(a);
     printf("%s\n", a);
