25d39806804948688f555fc2a3b749b809424cf3
nApi <106438187+Qriznapi@users.noreply.github.com>
2024-10-14 20:08:47 +0300

    Add files via upload

---
+++ 8sem/8n6.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print(int* a, size_t n)
+{
+    for (size_t i = 0; i < n; ++i)
+        printf("%i ", a[i]);
+    printf("\n");
+}
+
+void sort(int* a, size_t n, int (*compare)(int, int)) {
+    for (size_t j = 0; j < n; ++j) {
+        size_t min_index = j;
+        for (size_t i = j + 1; i < n; ++i) {
+            if (compare(a[i], a[min_index]) < 0) {
+                min_index = i;
+            }
+        }
+
+        int temp = a[j];
+        a[j] = a[min_index];
+        a[min_index] = temp;
+    }
+}
+
+int less(int a, int b) {
+    return a - b;
+}
+
+int greater(int a, int b) {
+    return b - a;
+}
+
+int last_digit_less(int a, int b) {
+    int last_digit_a = abs(a) % 10;
+    int last_digit_b = abs(b) % 10;
+    return last_digit_a - last_digit_b;
+}
+
+int main()
+{
+    int a[] = {32, 63, 29, 54, 81};
+
+    sort(a, 5, less);
+    print(a, 5); // Должен напечатать 29 32 54 63 81
+
+    sort(a, 5, greater);
+    print(a, 5); // Должен напечатать 81 63 54 32 29
+
+    sort(a, 5, last_digit_less);
+    print(a, 5); // Должен напечатать 81 32 63 54 29
+}
