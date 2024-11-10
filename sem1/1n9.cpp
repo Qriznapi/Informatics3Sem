d1db2fbf1c2b5e9ce0df0428b4ded9ac38485b72
nApi <106438187+Qriznapi@users.noreply.github.com>
2024-11-10 20:46:24 +0300

    Update 1n9.cpp

--- sem1/1n9.cpp
+++ sem1/1n9.cpp
@@ -6,16 +6,16 @@ std::string repeat(int n) {
         return "";
 
     std::string result;
-    //for (int i = 0; i < n; ++i) {
-    //    result += std::to_string(n);
-    //}
+    for (int i = 0; i < n; ++i) {
+        result += std::to_string(n);
+    }
     return result;
 }
 
-int main19() {
-    std::cout << repeat(5) << std::endl;   // Äîëæíî íàïå÷àòàòü 55555
-    std::cout << repeat(10) << std::endl;  // Äîëæíî íàïå÷àòàòü 10101010101010101010
-    std::cout << repeat(-1) << std::endl;  // Íå äîëæíî íè÷åãî ïå÷àòàòü
+int main() {
+    std::cout << repeat(5) << std::endl;   // Ã„Ã®Ã«Ã¦Ã­Ã® Ã­Ã Ã¯Ã¥Ã·Ã Ã²Ã Ã²Ã¼ 55555
+    std::cout << repeat(10) << std::endl;  // Ã„Ã®Ã«Ã¦Ã­Ã® Ã­Ã Ã¯Ã¥Ã·Ã Ã²Ã Ã²Ã¼ 10101010101010101010
+    std::cout << repeat(-1) << std::endl;  // ÃÃ¥ Ã¤Ã®Ã«Ã¦Ã­Ã® Ã­Ã¨Ã·Ã¥Ã£Ã® Ã¯Ã¥Ã·Ã Ã²Ã Ã²Ã¼
 
     return 0;
 }
